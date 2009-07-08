cf6ad5e3a750d51c65823fa420042321aa4d6350
Matt Rude <github@mattrude.com>
2009-07-08 00:59:11 -0400

    Code clean up and defult options reset.

--- Gallery2_ImageBlock.php
+++ Gallery2_ImageBlock.php
@@ -15,13 +15,6 @@ Thanks to all who came before who I stole code and comments from (particularly M
 
 // Put functions into one big function we'll call at the plugins_loaded
 // action. This ensures that all required plugin functions are defined.
-function sanitize($string) {
-    if (get_magic_quotes_gpc()) {
-        $string = stripslashes($string);
-    }
-    return $string;
-}
-
 function widget_G2_ImageBlock_init() {
   // Check for the required plugin functions. This will prevent fatal
   // errors occurring when you deactivate the dynamic-sidebar plugin.
@@ -33,9 +26,9 @@ function widget_G2_ImageBlock_init() {
   function widget_G2_ImageBlock_options() {
 	return array(
 	  'Title' => "Daily Image",
-      'Gallery URL' => "http://localhost/gallery",
+      'Gallery URL' => "http://putyourwebsitehere/gallery",
       'g2_blocks' => "dailyImage",
-      'g2_show' => "title",
+      'g2_show' => "title|date",
 	  'g2_itemId' => "",
 	  'g2_maxSize' => "",
 	  'g2_exactSize' => "",
@@ -44,7 +37,7 @@ function widget_G2_ImageBlock_init() {
     );
   }
 
-  // This is the function that outputs the actual  code.
+  // This is the function that outputs the actual code.
   function widget_G2_ImageBlock($args) {
     // $args is an array of strings that help widgets to conform to
     // the active theme: before_widget, before_title, after_widget,
@@ -77,37 +70,42 @@ $options['Gallery URL'] . '/main.php?g2_view=imageblock.External&g2_blocks=' . $
 ?>
 <?php
 /* This is the default way to load the daily image */
-//@readfile('http://www.theschierers.net/gallery/main.php?g2_view=imageblock.External&g2_blocks=dailyImage&g2_show=title'); ?>
-<?php 
+//@readfile('http://www.theschierers.net/gallery/main.php?g2_view=imageblock.External&g2_blocks=dailyImage&g2_show=title');
 /* This is the default way to load the random image */
-//@readfile('http://www.theschierers.net/gallery/main.php?g2_view=imageblock.External&g2_blocks=randomImage&g2_show=title');?>
-<!-- /G2 ImageBlock -->
-
-<?
-    echo $after_widget;
+//@readfile('http://www.theschierers.net/gallery/main.php?g2_view=imageblock.External&g2_blocks=randomImage&g2_show=title');
+  
+   echo $after_widget;
   }
+  
+// Gallery 2.3 sanitize function
+function sanitize($string) {
+    if (get_magic_quotes_gpc()) {
+        $string = stripslashes($string);
+    }
+    return $string;
+}
 
-  // This is the function that outputs the form to let the users edit
-  // the widget's title. It's an optional feature that users cry for.
-  function widget_G2_ImageBlock_control() {
-    // Each widget can store and retrieve its own options.
-    // Here we retrieve any options that may have been set by the user
-    // relying on widget defaults to fill the gaps.
-		if(($options = get_option('widget_G2_ImageBlock')) === FALSE) $options = array();
+// This is the function that outputs the form to let the users edit
+// the widget's title. It's an optional feature that users cry for.
+function widget_G2_ImageBlock_control() {
+  // Each widget can store and retrieve its own options.
+  // Here we retrieve any options that may have been set by the user
+  // relying on widget defaults to fill the gaps.
+  if(($options = get_option('widget_G2_ImageBlock')) === FALSE) $options = array();
     $options = array_merge(widget_G2_ImageBlock_options(), $options);
     unset($options[0]); //returned by get_option(), but we don't need it
-
+  
     // If user is submitting custom option values for this widget
     if ( $_POST['G2_ImageBlock-submit'] ) {
       // Remember to sanitize and format use input appropriately.
       foreach($options as $key => $value)
         if(array_key_exists('G2_ImageBlock-'.sanitize($key), $_POST))
           $options[$key] = strip_tags(stripslashes($_POST['G2_ImageBlock-'.sanitize($key)]));
-
+  
       // Save changes
       update_option('widget_G2_ImageBlock', $options);
     }
-
+  
     // Here is our little form segment. Notice that we don't need as
     // complete form. This will be embedded into the existing form.
     // Be sure you format your options to be valid HTML attributes
@@ -117,11 +115,11 @@ $options['Gallery URL'] . '/main.php?g2_view=imageblock.External&g2_blocks=' . $
     <? endforeach;
     echo '<input type="hidden" id="G2_ImageBlock-submit" name="G2_ImageBlock-submit" value="1" />';
   }
-
+  
   // This registers our widget so it appears with the other available
   // widgets and can be dragged and dropped into any active sidebars.
   register_sidebar_widget('G2 ImageBlock', 'widget_G2_ImageBlock');
-
+  
   // This registers our optional widget control form. Because of this
   // our widget will have a button that reveals a 300x100 pixel form.
   register_widget_control('G2 ImageBlock', 'widget_G2_ImageBlock_control', 220, 50 * count(widget_G2_ImageBlock_options()));
